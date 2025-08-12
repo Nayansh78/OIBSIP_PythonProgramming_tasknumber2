4f8a161e8b3b1e720e7069a9b529ae71d5597abe
Nayansh78 <nayan24153002@akgec.ac.in>
2025-08-12 18:38:32 +0530

    bmicalculatorr.py

--- bmicalculator.py
+++ bmicalculator.py
@@ -1,17 +1,16 @@
 # BMI-Calculator
-Weight=float(input("Enter the Weight in kg= "))
-Height=float(input("Enter the height in m = "))
-BMI=Weight/(Height*Height)
+Weight = float(input("Enter the Weight in kg = "))
+Height = float(input("Enter the height in m = "))
+BMI = Weight / (Height * Height)
 
-print(f"\nYour BMI is: {BMI}")
+print(f"\nYour BMI is: {BMI:.2f}")  # Rounded to 2 decimal places
 
-#Responses according to conditions 
-if BMI<18.5:
-  print("You are underweight")
-elif (BMI>=18.5, BMI<24.9):
-  print("You have a normal weight")
-elif BMI>24.9:
-  print("You are overweight ")
+# Responses according to conditions
+if BMI < 18.5:
+    print("You are underweight")
+elif BMI >= 18.5 and BMI < 24.9:
+    print("You have a normal weight")
+elif BMI >= 25 and BMI < 29.9:
+    print("You are overweight")
 else:
-  print("You are obese")
-  
+    print("You are obese")
